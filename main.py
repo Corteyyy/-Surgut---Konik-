ad160f9c06b44ab14bbd327712ef2e79fb9027d1
Yura Konik <konik_yv@mail.ru>
2020-11-21 22:28:25 +0500

    Git и желтые окружности

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys, random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class Window(QMainWindow):
+    global Ris
+    def __init__(self):
+        super(Window, self).__init__()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('Желтые кружки')
+        self.pushButton.clicked.connect(self.paint)
+        self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        for _ in range(random.randint(0, 12)):
+            qp.setBrush(QColor(254, 254, 2))
+            qp.drawEllipse(random.randint(0, 361), random.randint(0, 254), 60, 60)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wd = Window()
+    wd.show()
+    sys.exit(app.exec())
