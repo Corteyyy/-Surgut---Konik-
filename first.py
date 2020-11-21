2b28f8a7496081edf890af81608e54ef64197236
Yura Konik <konik_yv@mail.ru>
2020-11-21 21:40:27 +0500

    My first git-repo

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 import sys, math
 
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QLCDNumber
 
@@ -9,6 +10,21 @@ class Window(QMainWindow):
         super(Window, self).__init__()
         uic.loadUi('UI.ui', self)
         self.setWindowTitle('Желтые кружочки')
+        self.pushButton.clicked(self.paintEvent)
+
+    def paintEvent(self):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_flag(qp)
+        qp.end()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 30, 120, 30)
+        qp.setBrush(QColor(0, 255, 0))
+        qp.drawRect(30, 60, 120, 30)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 90, 120, 30)
 
 
 if __name__ == '__main__':
